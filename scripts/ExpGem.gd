dc31180ec0d95f3645010cb5b4403ee05b06a1f2
symbolic01 <jayamarks0@gmail.com>
2024-01-21 12:42:26 -0600

    enemy dead no push + gem recoil spike

--- scripts/ExpGem.gd
+++ scripts/ExpGem.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+onready var camera_node = get_node("/root/Main/Camera") # make autoload/global var
+
 var audio_samples := [
 	preload("res://sounds/gemsounds/v2/gemsound1.mp3"),
 	preload("res://sounds/gemsounds/v2/gemsound2.mp3"),
@@ -13,6 +15,11 @@ func _ready():
 	
 func _on_body_entered(body):
 	if body == Hero:
+		var recoil = Vector2(0,0)
+		recoil = (Hero.global_position - global_position).normalized() * 100
+		print(recoil)
+		var new_position = global_position + recoil
+
 		var random_note_index = randi() % audio_samples.size()
 		$Sprite.visible = false
 		$AudioStreamPlayer.set_stream(audio_samples[random_note_index])
@@ -21,3 +28,9 @@ func _on_body_entered(body):
 		
 func _on_audio_finished():
 	queue_free()
+	
+func _process(delta):
+	var recoil = Vector2(0,0)
+	recoil = (Hero.global_position - global_position).normalized() * 100
+	var new_position = global_position + recoil
+	self.z_index = int(global_position.y - camera_node.global_position.y)
