474d0cc18a6ff92281d71f3d6ae756b28a39e960
ckelimarks <98237632+ckelimarks@users.noreply.github.com>
2024-01-26 19:35:09 -0600

    Added Robot 8 direction hero character

--- scripts/Hero.gd
+++ scripts/Hero.gd
@@ -5,7 +5,7 @@ var pushing_strength = 300  # Adjust the pushing effect as needed
 var HP = 100.0
 var max_HP = 100.0
 
-onready var smooth_node = $Smoother
+onready var smooth_node = $Smoother/AnimatedSprite
 onready var healthbar_node = $Smoother/HealthNode/HeroHealth
 onready var camera_node = get_node("/root/Main/Camera")
 onready var main_node = get_node("/root/Main")
@@ -21,29 +21,46 @@ func _physics_process(delta):
 	var start_position = global_position  # Save the current position before moving
 	var sprite_start_position = smooth_node.position
 	var velocity = Vector2() # The player's movement vector
-
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var up = Input.is_action_pressed('ui_up')
+	var down = Input.is_action_pressed('ui_down')
 	# Get input from the player
-	if Input.is_action_pressed('ui_right'):
+	if right and down:
+		velocity += Vector2(speed, speed).normalized()
+		smooth_node.play("walk_se")
+	elif down and left:
+		velocity -= Vector2(speed, -speed).normalized()
+		smooth_node.play("walk_sw")
+	elif left and up:
+		velocity -= Vector2(speed, speed).normalized()
+		smooth_node.play("walk_nw")
+	elif up and right:
+		velocity += Vector2(speed, -speed).normalized()
+		smooth_node.play("walk_ne")
+	elif right:
 		velocity.x += 1
-		$Smoother/Sprite.flip_h = true
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-		$Smoother/Sprite.flip_h = false
-	if Input.is_action_pressed('ui_down'):
+		smooth_node.play("walk_e")
+	elif down:
 		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
+		smooth_node.play("walk_s")
+	elif left:
+		velocity.x -= 1
+		smooth_node.play("walk_w")
+	elif up:
 		velocity.y -= 1
+		smooth_node.play("walk_n")
 
 	# Move the player
 	# First, try to move normally.
 	var collision = move_and_collide(velocity.normalized() * speed * delta)
 
-	$Smoother/Sprite.modulate = Color(1, 1, 1, 1)
+	$Smoother/AnimatedSprite.modulate = Color(1, 1, 1, 1)
 	
 	if collision:
 		if collision.collider.is_in_group("enemies"):  # Check if the collider can be pushed
 			HP -= collision.collider.power
-			$Smoother/Sprite.modulate = Color(1, 0, 0, 1)
+			$Smoother/AnimatedSprite.modulate = Color(1, 0, 0, 1)
 			healthbar_node.value = HP / max_HP * 100
 			if HP <= 0:
 				main_node.reset()
